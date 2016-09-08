725247e2f185459f016b2018e7d63ccfa6858103
Sean Stephens <seanstephens29@gmail.com>
2016-09-08 15:53:35 -0600

    added pusher

--- addon/initializers/busy-pusher.js
+++ addon/initializers/busy-pusher.js
@@ -1,6 +1,44 @@
 export function initialize(app) {
   console.log(app);
-  // application.inject('route', 'foo', 'service:foo');
+
+  const BusyPusher = app;
+  window.BusyPusher = BusyPusher;
+
+	//if (BusyApp.SOCKETS) {
+		//setup socket listener
+		BusyPusher.pusher = new window.Pusher(BusyPusher.PUSHER_KEY, { cluster: BusyPusher.PUSHER_CLUSTER, encrypted: true });
+
+		// debug socket data
+		//if (BusyPusher.DEBUG_MODE) {
+			/**
+			* pusher connected
+			*/
+			BusyPusher.pusher.connection.bind('connected', function() {
+				window.console.log('pusher is connected');
+			});
+
+			/**
+			* pusher connecting in
+			*/
+			BusyPusher.pusher.connection.bind('connecting_in', function(delay) {
+				window.console.log("I haven't been able to establish a connection for this feature. I will try again in " + delay + " seconds.");
+			});
+
+			/**
+			* pusher state change
+			*/
+			BusyPusher.pusher.connection.bind('state_change', function(states) {
+				window.console.log('pusher state', states.current);
+			});
+
+			/**
+			* pusher error handler
+			*/
+			BusyPusher.pusher.connection.bind('error', function(err) {
+				window.console.log('pusher error', err);
+			});
+		//}
+	//}
 }
 
 export default {
